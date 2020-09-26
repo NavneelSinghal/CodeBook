7f0231dce121230db997f0ab13669d2ea8cd8b85
Navneel Singhal <navneel.singhal@ymail.com>
2020-09-26 20:38:22 +0530

    add another segtree

--- segtree.cpp
+++ segtree.cpp
@@ -1,3 +1,6 @@
+// implementation with optimal space
+// to allow for O(1) query for [0..n) by t[1], see the segtree below this one
+
 struct SegTree {
     // datatype of nodes of segment tree
     typedef int T;
@@ -44,6 +47,63 @@ struct SegTree {
         return combine(ra, rb);
     }
 };
+
+struct SegTree {
+    // datatype of nodes of segment tree
+    typedef int T;
+    // datatype of vector that's generating the segment tree
+    typedef int S;
+    // identity element of monoid
+    // if you have any issues with unit, define it outside the struct as a
+    // normal variable
+    static constexpr T unit = 0;
+    // node of segment tree from a value
+    T make_node(S val) { return val; }
+    // combine function - needs to be an associative function
+    T combine(T a, T b) { return a + b; }
+    // point update function - updating the element in the array
+    void update_val(T& a, S b) { a += b; }
+
+    vector<T> t;
+    int32_t n;
+    int32_t N;
+
+    SegTree(int32_t n = 0, T def = unit) : n(n) {
+        N = 1;
+        while (N < n) N <<= 1;
+        t.assign(N << 1, def);
+    }
+
+    SegTree(vector<S>& a, T def = unit) {
+        n = a.size();
+        N = 1;
+        while (N < n) N <<= 1;
+        t.assign(N << 2, unit);
+        for (int32_t i = 0; i < n; ++i) {
+            t[i + N] = make_node(a[i]);
+        }
+        for (int32_t i = n - 1; i; --i) {
+            t[i] = combine(t[i << 1], t[i << 1 | 1]);
+        }
+    }
+
+    void update(int32_t pos, S val) {
+        for (update_val(t[pos += N], val); pos >>= 1;) {
+            t[pos] = combine(t[pos << 1], t[pos << 1 | 1]);
+        }
+    }
+
+    T query(int32_t l, int32_t r) {
+        T ra = unit, rb = unit;
+        for (l += N, r += N; l < r; l >>= 1, r >>= 1) {
+            if (l & 1) ra = combine(ra, t[l++]);
+            if (r & 1) rb = combine(t[--r], rb);
+        }
+        return combine(ra, rb);
+    }
+};
+
+
 
 typedef struct {
     // datatype
