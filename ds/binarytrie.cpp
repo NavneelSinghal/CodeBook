7a2766f1531bb5c234f1bc6463493ffaed0ac00a
Navneel Singhal <navneel.singhal@ymail.com>
2020-11-08 11:13:19 +0530

    trie

--- ds/binarytrie.cpp
+++ ds/binarytrie.cpp
@@ -8,12 +8,12 @@ struct binaryTrie {
     int sz;
     vector<vector<int>> trie;
     vector<int> cnt;
-    vector<int> ends_here;
+    // vector<int> ends_here;
 
     // n is the number of total characters, so size needs to be 31 * n
     binaryTrie(int n) {
         sz = 0;
-        trie.assign(MAXLEN * (n + 2), vector<int>(2, -1));
+        trie.assign(2, vector<int>(MAXLEN * (n + 2), -1));
         // ends_here.resize(MAXLEN * (n + 2));
         cnt.resize(MAXLEN * (n + 2));
     }
@@ -21,7 +21,7 @@ struct binaryTrie {
     void insert(int n) {
         int node = ROOT;
         for (int i = MAXLEN - 1; ~i; --i) {
-            int& next_node = trie[node][(n >> i) & 1];
+            int& next_node = trie[(n >> i) & 1][node];
             if (next_node == -1) {
                 next_node = ++sz;
             }
@@ -34,7 +34,7 @@ struct binaryTrie {
     void remove(int n) { // assumes that n is already inside the trie - if not, then just do a simple traversal
         int node = ROOT;
         for (int i = MAXLEN - 1; ~i; --i) {
-            int next_node = trie[node][(n >> i) & 1];
+            int next_node = trie[(n >> i) & 1][node];
             node = next_node;
             cnt[node]--;
         }
@@ -43,7 +43,7 @@ struct binaryTrie {
     void removeAll(int n) { // assumes that n is already inside the trie - if not, then just do a simple traversal
         int node = ROOT;
         for (int i = MAXLEN - 1; ~i; --i) {
-            int next_node = trie[node][(n >> i) & 1];
+            int next_node = trie[(n >> i) & 1][node];
             node = next_node;
             cnt[node] = 0;
         }
@@ -55,11 +55,11 @@ struct binaryTrie {
         for (int i = MAXLEN - 1; ~i; --i) {
             ans <<= 1;
             int p = (n >> i) & 1;
-            if (trie[node][p] == -1 || cnt[trie[node][p]] <= 0) {
+            if (trie[p][node] == -1 || cnt[trie[p][node]] <= 0) {
                 ans ^= 1;
                 p ^= 1;
             }
-            node = trie[node][p];
+            node = trie[p][node];
         }
         return ans;
     }
@@ -71,17 +71,83 @@ struct binaryTrie {
             ans <<= 1;
             int p = (n >> i) & 1;
             p ^= 1;
-            if (trie[node][p] != -1 && cnt[trie[node][p]] > 0) {
+            if (trie[p][node] != -1 && cnt[trie[p][node]] > 0) {
                 ans ^= 1;
             } else {
                 p ^= 1;
             }
-            node = trie[node][p];
+            node = trie[p][node];
         }
         return ans;
     }
 
 };
+
+// if we need to make a binary trie on an array
+
+struct binaryTrie {
+    // MAX = number of nodes
+    static const int CHARSET = 2;
+    static const int MAXLEN = 31;
+    static const int ROOT = 0;
+    
+    int sz;
+    vector<vector<int>> trie;
+    vector<int> cnt;
+    vector<int> ends_here;
+    vector<int> a;
+
+    // n is the number of total characters, so size needs to be 31 * n
+    binaryTrie(int n) {
+        sz = 0;
+        trie.assign(2, vector<int>(MAXLEN * (n + 2), -1));
+        a.resize(n + 1);
+        ends_here.resize(MAXLEN * (n + 2));
+        cnt.resize(MAXLEN * (n + 2));
+    }
+
+    void insert(int n) {
+        int node = ROOT;
+        int to_insert = a[n];
+        for (int i = MAXLEN - 1; ~i; --i) {
+            int& next_node = trie[(to_insert >> i) & 1][node];
+            if (next_node == -1) {
+                next_node = ++sz;
+            }
+            node = next_node;
+            cnt[node]++;
+        }
+        ends_here[node] = n;
+    }
+
+    void remove(int n) { // assumes that n is already inside the trie - if not, then just do a simple traversal
+        int node = ROOT;
+        int to_remove = a[n];
+        for (int i = MAXLEN - 1; ~i; --i) {
+            node = trie[(to_remove >> i) & 1][node];
+            cnt[node]--;
+        }
+    }
+
+    pair<int, int> minxor(int n) {
+        int node = ROOT;
+        int ans = 0;
+        int to_use = a[n];
+        for (int i = MAXLEN - 1; ~i; --i) {
+            ans <<= 1;
+            int p = (to_use >> i) & 1;
+            int next_node = trie[p][node];
+            if (next_node == -1 || cnt[next_node] <= 0) {
+                ans ^= 1;
+                p ^= 1;
+            }
+            node = trie[p][node];
+        }
+        return make_pair(ans, ends_here[node]);
+    }
+
+};
+
 
 // when assuming all distinct, without deletion
 struct binaryTrie {
@@ -95,13 +161,13 @@ struct binaryTrie {
     // n is the number of total characters, so size needs to be 31 * n
     binaryTrie(int n) {
         sz = 1;
-        trie.assign(MAXLEN * (n + 2), vector<int>(2, -1));
+        trie.assign(2, vector<int>(MAXLEN * (n + 2), -1));
     }
 
     void insert(int n) {
         int node = ROOT;
         for (int i = MAXLEN - 1; ~i; --i) {
-            int& next_node = trie[node][(n >> i) & 1];
+            int& next_node = trie[(n >> i) & 1][node];
             if (next_node == -1) {
                 next_node = sz++;
             }
@@ -115,11 +181,11 @@ struct binaryTrie {
         for (int i = MAXLEN - 1; ~i; --i) {
             ans <<= 1;
             int p = (n >> i) & 1;
-            if (trie[node][p ^ 1] != -1) {
+            if (trie[p ^ 1][node] != -1) {
                 p ^= 1;
                 ans ^= 1;
             }
-            node = trie[node][p];
+            node = trie[p][node];
         }
         return ans;
     }
---
+++ ds/trie.cpp
@@ -0,0 +1,57 @@
+// might be better than below due to caching - change in the binary trie too maybe
+
+struct Trie {
+    static const int CHARSET = 26;
+    static const int ROOT = 0;
+    int sz;
+    vector<vector<int>> trie;
+    vector<int> freq;
+
+    // n is the total number of nodes we need to make in the trie
+    Trie(int n) {
+        sz = 1;
+        trie.assign(CHARSET, vector<int>(n + 2, -1));
+        freq.assign(n + 2, 0);
+    }
+
+    void insert(string& s) {
+        int node = ROOT;
+        for (auto c : s) {
+            int& next_node = trie[c - 'a'][node];
+            if (next_node == -1) {
+                next_node = sz++;
+            }
+            freq[next_node]++;
+            node = next_node;
+        }
+    }
+};
+
+struct Trie {
+    // MAX = number of nodes
+    static const int CHARSET = 26;
+    static const int ROOT = 0;
+    int sz;
+    vector<vector<int>> trie;
+    vector<int> freq;
+
+    // n is the total number of nodes we need to make in the trie
+    Trie(int n) {
+        sz = 1;
+        trie.assign(n + 2, vector<int>(CHARSET, -1));
+        freq.assign(n + 2, 0);
+    }
+
+    void insert(string& s) {
+        int node = ROOT;
+        for (auto c : s) {
+            int& next_node = trie[node][c - 'a'];
+            if (next_node == -1) {
+                next_node = sz++;
+            }
+            freq[next_node]++;
+            node = next_node;
+        }
+    }
+
+};
