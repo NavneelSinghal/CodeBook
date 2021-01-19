5afaad3f2a038c09acf38a97ed81885beb28c1e7
Navneel Singhal <navneel.singhal@ymail.com>
2021-01-19 18:50:01 +0530

    add simple fast io code for linking to

---
+++ misc/short-fast-io/fast-io.py
@@ -0,0 +1,25 @@
+# fast IO
+import sys
+input = sys.stdin.readline
+def print(x, end='\n'):
+    sys.stdout.write(str(x) + end)
+
+# IO helpers
+def get_int():
+    return int(input())
+def get_list_ints():
+    return list(map(int, input().split()))
+def get_char_list():
+    s = input()
+    return list(s[:len(s) - 1])
+def get_tuple_ints():
+    return tuple(map(int, input().split()))
+def print_iterable(p):
+    print(" ".join(map(str, p)))
+
+# code goes here
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
