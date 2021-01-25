2556870b51352c94199c902d70ec2f28f7e339bc
Navneel Singhal <navneel.singhal@ymail.com>
2021-01-25 10:11:17 +0530

    update dsu

--- ds/dsu.cpp
+++ ds/dsu.cpp
@@ -1,22 +1,46 @@
-// dsu implementation
-vector<int32_t> par(maxn), siz(maxn);
+struct dsu {
+    int n;
+    vector<int32_t> par, siz;
 
-void make_set(int32_t v) {
-    par[v] = v;
-    siz[v] = 1;
-}
+    dsu(int n) : n(n), par(n), siz(n) {
+        for (int i = 0; i < n; ++i) {
+            make_set(i);
+        }
+    };
 
-int32_t find_set(int32_t v) {
-    if (v == par[v]) return v;
-    return par[v] = find_set(par[v]);
-}
-
-void union_sets(int32_t a, int32_t b) {
-    a = find_set(a);
-    b = find_set(b);
-    if (a != b) {
-        if (siz[a] < siz[b]) swap(a, b);
-        par[b] = a;
-        siz[a] += siz[b];
+    inline void make_set(int32_t v) {
+        par[v] = v;
+        siz[v] = 1;
     }
-}
+
+    int32_t find_set(int32_t v) {
+        if (v == par[v]) return v;
+        return par[v] = find_set(par[v]);
+    }
+
+    void union_sets(int32_t a, int32_t b) {
+        a = find_set(a);
+        b = find_set(b);
+        if (a != b) {
+            if (siz[a] < siz[b]) swap(a, b);
+            par[b] = a;
+            siz[a] += siz[b];
+        }
+    }
+
+    vector<vector<int>> components() {
+        vector<int> representative(n), component_size(n);
+        for (int i = 0; i < n; ++i) {
+            representative[i] = find_set(i);
+            component_size[representative[i]]++;
+        }
+        vector<vector<int>> components(n);
+        for (int i = 0; i < n; ++i) components[i].reserve(component_size[i]);
+        for (int i = 0; i < n; ++i) components[representative[i]].push_back(i);
+        components.erase(
+            remove_if(components.begin(), components.end(),
+                      [](const vector<int>& v) { return v.empty(); }),
+            components.end());
+        return components;
+    }
+};
