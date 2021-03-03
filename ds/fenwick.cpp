5c329f67998638a297f20e1a157f38a01ba734f3
Navneel Singhal <navneel.singhal@ymail.com>
2021-03-04 00:13:22 +0530

    generic fenwick tree

--- ds/fenwick.cpp
+++ ds/fenwick.cpp
@@ -1,21 +1,22 @@
 // 1-based indexing
+template <typename T>
 struct Fenwick {
     int n;
-    vector<int> t;
+    vector<T> t;
     Fenwick(int n) : n(n), t(n + 1) {}
-
     // prefix_sum[0..i]
-    int query(int i) {
-        int s = 0;
+    T query(int i) {
+        T s = 0;
         while (i) {
             s += t[i];
             i -= i & (-i);
         }
         return s;
     }
-
+    // range query
+    T query(int l, int r) { return query(r) - query(l - 1); }
     // increase a[i] by v
-    void update(int v, int i) {
+    void update(int i, T v) {
         while (i <= n) {
             t[i] += v;
             i += i & (-i);
