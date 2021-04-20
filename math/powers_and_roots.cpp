0dc379f4e1b468a92e996413703265346da14bef
Navneel Singhal <navneel.singhal@ymail.com>
2021-04-20 15:57:14 +0530

    add wavelet tree and segtree beats

--- math/powers_and_roots.cpp
+++ math/powers_and_roots.cpp
@@ -5,7 +5,8 @@ const long long linf = 3e18;
 long long pwr_mod(long long a, long long n) {
     long long ans = 1;
     while (n) {
-        if (n & 1) ans = (ans * a) % mod;
+        if (n & 1)
+            ans = (ans * a) % mod;
         a = (a * a) % mod;
         n >>= 1;
     }
@@ -17,8 +18,10 @@ template <typename T>
 T pwr(T a, long long n) {
     T ans = 1;
     while (n) {
-        if (n & 1) ans *= a;
-        if (n > 1) a *= a;
+        if (n & 1)
+            ans *= a;
+        if (n > 1)
+            a *= a;
         n >>= 1;
     }
     return ans;
@@ -49,8 +52,10 @@ long long power_overflow(long long a, long long n) {
 inline long long isqrt(long long n) {
     long long sq = (long long)sqrtl((long double)n) - 2;
     sq = max(sq, 0LL);
-    while (sq * sq < n) sq++;
-    if ((sq * sq) == n) return sq;
+    while (sq * sq < n)
+        sq++;
+    if ((sq * sq) == n)
+        return sq;
     return sq - 1;
 }
 
@@ -58,8 +63,8 @@ inline long long isqrt(long long n) {
 // keep a global struct
 
 struct KthRoot {
-    using u32 = unsigned int;
-    using u64 = unsigned long long int;
+    using u32 = unsigned;
+    using u64 = unsigned long long;
     vector<u64> pow[65];  // pow[k][x] = pow(x+2,k)  (k >= 4)
     KthRoot() {
         for (u32 t = 2; t < (1 << 16); t++) {
@@ -67,12 +72,14 @@ struct KthRoot {
             s = s * s;
             for (int k = 4;; k++) {
                 pow[k].push_back(s);
-                if (__builtin_umulll_overflow(s, t, &s)) break;
+                if (__builtin_umulll_overflow(s, t, &s))
+                    break;
             }
         }
     }
     u64 sqrt(u64 n) const {
-        if (n == -1ull) return u32(-1);
+        if (n == -1ull)
+            return u32(-1);
         u64 x = std::sqrt((double)n);
         return x * x > n ? x - 1 : x;
     }
@@ -81,16 +88,20 @@ struct KthRoot {
         for (int s = 63; s >= 0; s -= 3) {
             x <<= 1;
             y = 3 * x * (x + 1) + 1;
-            if (y <= (n >> s)) n -= y << s, ++x;
+            if (y <= (n >> s))
+                n -= y << s, ++x;
         }
         return x;
     }
     u64 operator()(u64 n, int k) {
         assert(k >= 1);
-        if (k == 1 || n == 0) return n;
-        if (k == 2) return sqrt(n);
-        if (k == 3) return cbrt(n);
+        if (k == 1 || n == 0)
+            return n;
+        if (k == 2)
+            return sqrt(n);
+        if (k == 3)
+            return cbrt(n);
         auto ub = upper_bound(pow[k].begin(), pow[k].end(), n);
         return (ub - pow[k].begin()) + 1;
     }
-};
+} roots;
