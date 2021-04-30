38338508d6653f4d335e7c9cf502a450fd33a129
Navneel Singhal <navneel.singhal@ymail.com>
2021-04-30 23:44:38 +0530

    add stuff

--- math/extended_euclid.cpp
+++ math/extended_euclid.cpp
@@ -1,3 +1,21 @@
+template <typename T>
+array<T, 3> ext_gcd(T a, T b) {
+    T old_r = a, r = b, old_s = 1, s = 0, old_t = 0, t = 1;
+    while (r != 0) {
+        T quot = old_r / r;
+        tie(old_r, r) = make_pair(r, old_r - quot * r);
+        tie(old_s, s) = make_pair(s, old_s - quot * s);
+        tie(old_t, t) = make_pair(t, old_t - quot * t);
+    }
+    if (old_r < 0) {
+        old_r *= -1;
+        old_s *= -1;
+        old_t *= -1;
+    }
+    // gcd, coeff of a, coeff of b
+    return {old_r, old_s, old_t};
+}
+
 // stores result for sol to ax + by = g in x and y
 int gcd(int a, int b, int& x, int& y) {
     x = 1, y = 0;
