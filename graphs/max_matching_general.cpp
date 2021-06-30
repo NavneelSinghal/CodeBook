3fdb650d22791c84bd880d4e0091d5123f064417
Navneel Singhal <navneel.singhal@ymail.com>
2021-06-30 10:08:31 +0530

    get_matching in general matching

--- graphs/max_matching_general.cpp
+++ graphs/max_matching_general.cpp
@@ -1,6 +1,3 @@
-#include <bits/stdc++.h>
-using namespace std;
-
 class MaximumMatching {
     /*
      * Maximum Cardinality Matching in General Graphs.
@@ -58,14 +55,12 @@ class MaximumMatching {
     struct DisjointSetUnion {
         DisjointSetUnion() {}
         DisjointSetUnion(int N) : par(N) {
-            for (int i = 0; i < N; ++i)
-                par[i] = i;
+            for (int i = 0; i < N; ++i) par[i] = i;
         }
         int find(int u) { return par[u] == u ? u : (par[u] = find(par[u])); }
         void unite(int u, int v) {
             u = find(u), v = find(v);
-            if (u != v)
-                par[v] = u;
+            if (u != v) par[v] = u;
         }
         vector<int> par;
     };
@@ -73,16 +68,13 @@ class MaximumMatching {
    public:
     MaximumMatching(int N, const vector<Edge> &in)
         : N(N), NH(N >> 1), ofs(N + 2, 0), edges(in.size() * 2) {
-        for (auto &e : in)
-            ofs[e.from + 1] += 1, ofs[e.to + 1] += 1;
-        for (int i = 1; i <= N + 1; ++i)
-            ofs[i] += ofs[i - 1];
+        for (auto &e : in) ofs[e.from + 1] += 1, ofs[e.to + 1] += 1;
+        for (int i = 1; i <= N + 1; ++i) ofs[i] += ofs[i - 1];
         for (auto &e : in) {
             edges[ofs[e.from]++] = e;
             edges[ofs[e.to]++] = {e.to, e.from};
         }
-        for (int i = N + 1; i > 0; --i)
-            ofs[i] = ofs[i - 1];
+        for (int i = N + 1; i > 0; --i) ofs[i] = ofs[i - 1];
         ofs[0] = 0;
     }
 
@@ -92,12 +84,18 @@ class MaximumMatching {
         while (match * 2 + 1 < N) {
             reset_count();
             bool has_augmenting_path = do_edmonds_search();
-            if (!has_augmenting_path)
-                break;
+            if (!has_augmenting_path) break;
             match += find_maximal();
             clear();
         }
         return match;
+    }
+
+    vector<Edge> get_edges() {
+        vector<Edge> ans;
+        for (int i = 1; i <= N; ++i)
+           if (mate[i] > i) ans.push_back(Edge{i, mate[i]});
+        return ans;
     }
 
    private:
@@ -111,14 +109,10 @@ class MaximumMatching {
 
     void clear() {
         que.clear();
-        for (int u = 1; u <= N; ++u)
-            potential[u] = 1;
-        for (int u = 1; u <= N; ++u)
-            dsu.par[u] = u;
-        for (int t = time_current_; t <= N / 2; ++t)
-            list.head[t] = -1;
-        for (int u = 1; u <= N; ++u)
-            blossom.head[u] = -1;
+        for (int u = 1; u <= N; ++u) potential[u] = 1;
+        for (int u = 1; u <= N; ++u) dsu.par[u] = u;
+        for (int t = time_current_; t <= N / 2; ++t) list.head[t] = -1;
+        for (int u = 1; u <= N; ++u) blossom.head[u] = -1;
     }
 
     // first phase
@@ -138,11 +132,9 @@ class MaximumMatching {
         label[mate[bx]] = label[mate[by]] = h;
         int lca = -1;
         while (1) {
-            if (mate[by] != 0)
-                swap(bx, by);
+            if (mate[by] != 0) swap(bx, by);
             bx = lca = dsu.find(link[bx].from);
-            if (label[mate[bx]] == h)
-                break;
+            if (label[mate[bx]] == h) break;
             label[mate[bx]] = h;
         }
         for (auto bv : {dsu.par[x], dsu.par[y]}) {
@@ -169,12 +161,10 @@ class MaximumMatching {
                 if (label[y] > 0) {  // outer blossom/vertex
                     int time_next = (px + potential[y]) >> 1;
                     if (lx != label[y]) {
-                        if (time_next == time_current_)
-                            return true;
+                        if (time_next == time_current_) return true;
                         time_augment_ = min(time_next, time_augment_);
                     } else {
-                        if (bx == dsu.find(y))
-                            continue;
+                        if (bx == dsu.find(y)) continue;
                         if (time_next == time_current_) {
                             contract(x, y);
                             bx = dsu.find(x);
@@ -198,8 +188,7 @@ class MaximumMatching {
         const int time_lim = min(NH + 1, time_augment_);
         for (++time_current_; time_current_ <= time_lim; ++time_current_) {
             dsu_changelog_size_ = dsu_changelog_last_;
-            if (time_current_ == time_lim)
-                break;
+            if (time_current_ == time_lim) break;
             bool updated = false;
             for (int h = list.head[time_current_]; h >= 0; h = list.next[h]) {
                 auto &e = edges[h];
@@ -208,8 +197,7 @@ class MaximumMatching {
                     // Case: outer -- (free => inner => outer)
                     if (potential[x] + potential[y] != (time_current_ << 1))
                         continue;
-                    if (dsu.find(x) == dsu.find(y))
-                        continue;
+                    if (dsu.find(x) == dsu.find(y)) continue;
                     if (label[x] != label[y]) {
                         time_augment_ = time_current_;
                         return false;
@@ -222,8 +210,7 @@ class MaximumMatching {
                 }
             }
             list.head[time_current_] = -1;
-            if (updated)
-                return false;
+            if (updated) return false;
         }
         return time_current_ > NH;
     }
@@ -238,13 +225,10 @@ class MaximumMatching {
                 label[u] = kFree;
         }
         while (1) {
-            if (find_augmenting_path())
-                break;
+            if (find_augmenting_path()) break;
             bool maximum = adjust_dual_variables();
-            if (maximum)
-                return false;
-            if (time_current_ == time_augment_)
-                break;
+            if (maximum) return false;
+            if (time_current_ == time_augment_) break;
         }
         for (int u = 1; u <= N; ++u) {
             if (label[u] > 0)
@@ -260,8 +244,7 @@ class MaximumMatching {
     void rematch(int v, int w) {
         int t = mate[v];
         mate[v] = w;
-        if (mate[t] != v)
-            return;
+        if (mate[t] != v) return;
         if (link[v].to == dsu.find(link[v].to)) {
             mate[t] = link[v].from;
             rematch(mate[t], t);
@@ -276,12 +259,10 @@ class MaximumMatching {
         int px = potential[x], lx = label[bx];
         for (int eid = ofs[x]; eid < ofs[x + 1]; ++eid) {
             int y = edges[eid].to;
-            if (px + potential[y] != 0)
-                continue;
+            if (px + potential[y] != 0) continue;
             int by = dsu.find(y), ly = label[by];
             if (ly > 0) {  // outer
-                if (lx >= ly)
-                    continue;
+                if (lx >= ly) continue;
                 int stack_beg = stack_last_;
                 for (int bv = by; bv != bx; bv = dsu.find(link[bv].from)) {
                     int bw = dsu.find(mate[bv]);
@@ -293,8 +274,7 @@ class MaximumMatching {
                     int bv = stack[--stack_last_];
                     for (int v = blossom.head[bv]; v >= 0;
                          v = blossom.next[v]) {
-                        if (!dfs_augment(v, bx))
-                            continue;
+                        if (!dfs_augment(v, bx)) continue;
                         stack_last_ = stack_beg;
                         return true;
                     }
@@ -311,8 +291,7 @@ class MaximumMatching {
                 link[bz] = {x, y};
                 label[bz] = outer_id_++;
                 for (int v = blossom.head[bz]; v >= 0; v = blossom.next[v]) {
-                    if (dfs_augment(v, bz))
-                        return true;
+                    if (dfs_augment(v, bz)) return true;
                 }
             }
         }
@@ -321,8 +300,7 @@ class MaximumMatching {
 
     int find_maximal() {
         // discard blossoms whose potential is 0.
-        for (int u = 1; u <= N; ++u)
-            dsu.par[u] = u;
+        for (int u = 1; u <= N; ++u) dsu.par[u] = u;
         for (int i = 0; i < dsu_changelog_size_; ++i) {
             dsu.par[dsu_changelog[i].v] = dsu_changelog[i].par;
         }
@@ -334,12 +312,10 @@ class MaximumMatching {
         for (int u = 1; u <= N; ++u)
             if (!mate[u]) {
                 int bu = dsu.par[u];
-                if (label[bu] != kFree)
-                    continue;
+                if (label[bu] != kFree) continue;
                 label[bu] = outer_id_++;
                 for (int v = blossom.head[bu]; v >= 0; v = blossom.next[v]) {
-                    if (!dfs_augment(v, bu))
-                        continue;
+                    if (!dfs_augment(v, bu)) continue;
                     ret += 1;
                     break;
                 }
@@ -392,26 +368,4 @@ class MaximumMatching {
 };
 
 using Edge = MaximumMatching::Edge;
-
-void solve(int case_no) {
-    int n, x;
-    cin >> n;
-    vector<Edge> edges(n);
-    for (int i = 1; i <= n; ++i) {
-        cin >> x;
-        edges[i] = {i, x};
-    }
-    cout << MaximumMatching(n, edges).maximum_matching() << endl;
-}
-
-signed main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    int t = 1;
-    for (int _t = 1; _t <= t; _t++) {
-        solve(_t);
-    }
-    return 0;
-}
 
