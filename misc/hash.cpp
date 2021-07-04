dda65d763eb17c9d3de2eb64ca0f33dedbc82179
Navneel Singhal <navneel.singhal@ymail.com>
2021-07-04 16:53:39 +0530

    fix hash

--- misc/hash.cpp
+++ misc/hash.cpp
@@ -1,4 +1,5 @@
 struct custom_hash {
+    using ull = uint64_t;
     // http://xorshift.di.unimi.it/splitmix64.c
     static ull splitmix64(ull x) {
         x += 0x9e3779b97f4a7c15;
@@ -6,13 +7,11 @@ struct custom_hash {
         x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
         return x ^ (x >> 31);
     }
-
     size_t operator()(ull x) const {
         static const ull FIXED_RANDOM =
             std::chrono::steady_clock::now().time_since_epoch().count();
         return splitmix64(x + FIXED_RANDOM);
     }
-
     size_t operator()(std::pair<int, int> p) const {
         static const ull FIXED_RANDOM =
             std::chrono::steady_clock::now().time_since_epoch().count();
