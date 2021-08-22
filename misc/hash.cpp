36bb2e073de2a3c1fda520f4d7ef3f8fe0cb9590
Navneel Singhal <navneel.singhal.iitd@gmail.com>
2021-08-23 02:31:41 +0530

    revert binary search, hash

--- misc/hash.cpp
+++ misc/hash.cpp
@@ -1,20 +1,62 @@
-struct custom_hash {
-    using ull = uint64_t;
+namespace hashing {
+    using ull = std::uint64_t;
+    static const ull FIXED_RANDOM =
+        std::chrono::steady_clock::now().time_since_epoch().count();
+
+    template <class T, class D = void>
+    struct custom_hash {};
+
+    // https://www.boost.org/doc/libs/1_55_0/doc/html/hash/combine.html
+    template <class T>
+    inline void hash_combine(ull& seed, const T& v) {
+        custom_hash<T> hasher;
+        seed ^= hasher(v) + 0x9e3779b97f4a7c15 + (seed << 12) + (seed >> 4);
+    };
+
     // http://xorshift.di.unimi.it/splitmix64.c
-    static ull splitmix64(ull x) {
-        x += 0x9e3779b97f4a7c15;
-        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
-        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
-        return x ^ (x >> 31);
-    }
-    size_t operator()(ull x) const {
-        static const ull FIXED_RANDOM =
-            std::chrono::steady_clock::now().time_since_epoch().count();
-        return splitmix64(x + FIXED_RANDOM);
-    }
-    size_t operator()(std::pair<int, int> p) const {
-        static const ull FIXED_RANDOM =
-            std::chrono::steady_clock::now().time_since_epoch().count();
-        return splitmix64(p.first * 31 + p.second + FIXED_RANDOM);
-    }
-};
+    template <class T>
+    struct custom_hash<
+        T, typename std::enable_if<std::is_integral<T>::value>::type> {
+        ull operator()(T v) const {
+            ull x = v + 0x9e3779b97f4a7c15 + FIXED_RANDOM;
+            x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+            x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+            return x ^ (x >> 31);
+        }
+    };
+
+    template <class T>
+    struct custom_hash<T,
+                       std::void_t<decltype(std::begin(std::declval<T>()))>> {
+        ull operator()(const T& a) const {
+            ull value = FIXED_RANDOM;
+            for (auto& x : a) hash_combine(value, x);
+            return value;
+        }
+    };
+
+    template <class... T>
+    struct custom_hash<std::tuple<T...>> {
+        ull operator()(const std::tuple<T...>& a) const {
+            ull value = FIXED_RANDOM;
+            std::apply(
+                [&value](T const&... args) {
+                    (hash_combine(value, args), ...);
+                },
+                a);
+            return value;
+        }
+    };
+
+    template <class T, class U>
+    struct custom_hash<std::pair<T, U>> {
+        ull operator()(const std::pair<T, U>& a) const {
+            ull value = FIXED_RANDOM;
+            hash_combine(value, a.first);
+            hash_combine(value, a.second);
+            return value;
+        }
+    };
+
+};  // namespace hashing
+
