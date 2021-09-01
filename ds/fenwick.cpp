d7faff5575a6e3f85fc02ae1966e9f778ec2ea69
Navneel Singhal <navneel.singhal.iitd@gmail.com>
2021-09-01 06:40:33 +0530

    add binary search to fenwick

--- ds/fenwick.cpp
+++ ds/fenwick.cpp
@@ -22,5 +22,39 @@ struct Fenwick {
             i += i & (-i);
         }
     }
+    // if next = true, returns index of first element that is > or >=
+    // if next = false, returns index of last element that is < or <=
+    template <bool next, bool strict>
+    int search(T v) {
+        if (v == 0) {
+            if constexpr (!next)
+                return -int(strict);
+            else if constexpr (!strict)
+                return 0;
+        } else if (v < 0) {
+            return int(next) - 1;
+        }
+        T sum = 0;
+        int pos = 0;
+        for (int i = __lg(n); i >= 0; --i) {
+            if (pos + (1 << i) <= n) {
+                bool b = false;
+                T s = sum + t[pos + (1 << i)];
+                if constexpr (next != strict) {
+                    b = s < v;
+                } else {
+                    b = s <= v;
+                }
+                if (b) {
+                    sum = s;
+                    pos += (1 << i);
+                }
+            }
+        }
+        if constexpr (next)
+            return pos + 1;
+        else
+            return pos;
+    }
 };
 
