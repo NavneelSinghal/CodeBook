ff60ace9107dd19ef8ba81e175003f567d2a9070
Navneel Singhal <navneel.singhal.iitd@gmail.com>
2021-10-16 23:55:48 +0530

    add overload for string literals

--- misc/io.cpp
+++ misc/io.cpp
@@ -51,9 +51,10 @@ struct IO {
 
     template <class T>
     struct is_string {
-        static constexpr bool value = std::is_same_v<T, std::string> ||
-                                      std::is_same_v<T, const char*> ||
-                                      std::is_same_v<T, char*>;
+        static constexpr bool value =
+            std::is_same_v<T, std::string> || std::is_same_v<T, const char*> ||
+            std::is_same_v<T, char*> || std::is_same_v<std::decay_t<T>, char*>;
+        ;
     };
 
     template <class T, class D = void>
