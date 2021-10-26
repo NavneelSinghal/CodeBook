d28d755bf7b376ba5150d2e0b4254fbe12d97fc4
Navneel Singhal <navneel.singhal.iitd@gmail.com>
2021-10-26 23:27:58 +0530

    fix pragmas

--- misc/pragmas.cpp
+++ misc/pragmas.cpp
@@ -1,4 +1,4 @@
 #pragma GCC optimize("O3,unroll-loops")
-#pragma GCC target("avx2,bmi2,popcnt,lzcnt")
-// #pragma GCC target("avx2,sse4.2,bmi2,popcnt,lzcnt")
+#pragma GCC target("avx2,bmi,bmi2,popcnt,lzcnt")
+// #pragma GCC target("avx2,sse4.2,bmi,bmi2,popcnt,lzcnt")
 
