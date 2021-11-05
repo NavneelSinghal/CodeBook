38e21a9ca2311abff274fec54963b48cd5b384d9
Navneel Singhal <navneel.singhal.iitd@gmail.com>
2021-11-05 17:02:15 +0530

    add aligned malloc stuff for codeforces

--- misc/binsearch_array.cpp
+++ misc/binsearch_array.cpp
@@ -7,6 +7,61 @@ constexpr std::size_t hardware_constructive_interference_size = 64;
 constexpr std::size_t hardware_destructive_interference_size = 64;
 #endif
 
+
+#ifdef __cpp_lib_hardware_interference_size
+using std::hardware_constructive_interference_size;
+using std::hardware_destructive_interference_size;
+#else
+// 64 bytes on x86-64 │ L1_CACHE_BYTES │ L1_CACHE_SHIFT │ __cacheline_aligned │
+constexpr std::size_t hardware_constructive_interference_size = 64;
+constexpr std::size_t hardware_destructive_interference_size = 64;
+#endif
+
+/*
+// if submitting on codeforces:
+template <class T>
+struct BinSearcher {
+    static constexpr std::size_t cache_line_size =
+        hardware_destructive_interference_size;
+    static constexpr std::size_t block_size = cache_line_size / sizeof(T);
+    static constexpr std::size_t alignment =
+        std::max(cache_line_size, alignof(T));
+    T* b;
+    std::vector<int> index;
+    int n;
+    BinSearcher(const std::vector<T>& a)
+        : b(static_cast<T*>(
+            _aligned_malloc((sizeof(T) * (a.size() + 1) + alignment - 1) /
+                             alignment * alignment, alignment))),
+          index(a.size()),
+          n((int)a.size()) {
+        auto rearrange = [&](auto&& self, int a_index, int b_index) -> int {
+            if (b_index <= n) {
+                a_index = self(self, a_index, 2 * b_index);
+                index[b_index] = a_index;
+                b[b_index] = a[a_index++];
+                a_index = self(self, a_index, 2 * b_index + 1);
+            }
+            return a_index;
+        };
+        index[0] = n;
+        rearrange(rearrange, 0, 1);
+    }
+    ~BinSearcher() { _aligned_free(b); }
+    template <class F>
+    int find_first_false(F&& f) {
+        int i = 1;
+        while (i <= n) {
+            if constexpr (block_size != 0)
+                __builtin_prefetch(b + i * block_size);
+            i = 2 * i + f(b[i]);
+        }
+        i >>= __builtin_ffs(~i);
+        return index[i];
+    }
+};
+*/
+
 template <class T>
 struct BinarySearcher {
     static constexpr std::size_t cache_line_size =
