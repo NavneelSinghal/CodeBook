c7768eea96a0dca6eb808bf881224b2eec85ade7
Navneel Singhal <navneel.singhal.iitd@gmail.com>
2022-01-20 20:31:14 +0530

    fix linear diophantine equations, add gaussian elimination, update snippets, add convolution mod 64

--- math/diophantine.cpp
+++ math/diophantine.cpp
@@ -1,5 +1,6 @@
 template <typename T = int>
-T find_all_solutions(T a, T b, T c, T minx, T maxx, T miny, T maxy) {
+tuple<T, T, ll> find_all_solutions(T a, T b, T c, T minx, T maxx, T miny,
+                                   T maxy) {
     // stores result for sol to ax + by = g in x and y
     auto gcd = [](T a, T b, T& x, T& y) {
         x = 1, y = 0;
@@ -17,7 +18,7 @@ T find_all_solutions(T a, T b, T c, T minx, T maxx, T miny, T maxy) {
     };
     // solves ax + by = c and stores results in x, y - x, y are still changed if
     // it returns false
-    auto find_any_sol = [](T a, T b, T c, T& x, T& y, T& g) {
+    auto find_any_sol = [&](T a, T b, T c, T& x, T& y, T& g) {
         g = gcd(abs(a), abs(b), x, y);
         if (c % g) return false;
         x *= c / g;
@@ -33,16 +34,17 @@ T find_all_solutions(T a, T b, T c, T minx, T maxx, T miny, T maxy) {
     };
 
     T x, y, g;
-    if (!find_any_solution(a, b, c, x, y, g)) return 0;
+    if (!find_any_sol(a, b, c, x, y, g)) return {0, 0, 0};
     a /= g;
     b /= g;
+    c /= g;
 
     int sign_a = a > 0 ? +1 : -1;
     int sign_b = b > 0 ? +1 : -1;
 
     shift_solution(x, y, a, b, (minx - x) / b);
     if (x < minx) shift_solution(x, y, a, b, sign_b);
-    if (x > maxx) return 0;
+    if (x > maxx) return {0, 0, 0};
     T lx1 = x;
 
     shift_solution(x, y, a, b, (maxx - x) / b);
@@ -51,7 +53,7 @@ T find_all_solutions(T a, T b, T c, T minx, T maxx, T miny, T maxy) {
 
     shift_solution(x, y, a, b, -(miny - y) / a);
     if (y < miny) shift_solution(x, y, a, b, -sign_a);
-    if (y > maxy) return 0;
+    if (y > maxy) return {0, 0, 0};
     T lx2 = x;
 
     shift_solution(x, y, a, b, -(maxy - y) / a);
@@ -64,7 +66,9 @@ T find_all_solutions(T a, T b, T c, T minx, T maxx, T miny, T maxy) {
 
     // lx is the first value of x in the final interval
 
-    if (lx > rx) return 0;
-    return (rx - lx) / abs(b) + 1;
+    T ly = (c - lx * a) / b;
+
+    if (lx > rx) return {0, 0, 0};
+    return {lx, ly, (rx - lx) / abs(b) + 1};
 }
 
