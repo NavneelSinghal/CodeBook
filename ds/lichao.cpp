cbcb5f9606d29528c7d8b48a2077b65805f0fc8b
Navneel Singhal <navneel.singhal.iitd@gmail.com>
2022-03-04 19:42:42 +0530

    make lichao better

--- ds/lichao.cpp
+++ ds/lichao.cpp
@@ -1,9 +1,16 @@
-// BUF should be enough to store O((N + Q) log (range)) nodes
-// Don't make more than one template specialization for the lichao tree since
-// otherwise the buffer will be pretty huge
-// uses two buffers - one for each template
-template <class T, T x_low, T x_high, T id, int BUF>
+template <class T>
+struct deque_allocator {
+    std::deque<T> d;
+    template <typename... Args>
+    T* allocate(Args&&... args) {
+        d.emplace_back(args...);
+        return &d.back();
+    }
+};
+
+template <class Info>
 struct LiChaoTree {
+    using T = typename Info::T;
     struct Line {
         T a, b;
         Line(T a, T b) : a(a), b(b) {}
@@ -21,13 +28,9 @@ struct LiChaoTree {
     };
 
     template <class U>
-    Node* newNode(const U& x) {
-        static int bufSize = BUF;
-        static Node buf[BUF];
-
-        assert(bufSize);
-        buf[--bufSize] = Node(x);
-        return &buf[bufSize];
+    Node* newNode(U&& x) {
+        static deque_allocator<Node> allocator;
+        return allocator.allocate(std::forward<Node>(x));
     }
 
     Node* root;
@@ -38,22 +41,22 @@ struct LiChaoTree {
                  const T& x_r) {
         if (!t) return newNode(x);
         auto t_l = t->x(l), t_r = t->x(r);
-        if (t_l <= x_l && t_r <= x_r) {
+        if (!Info::cmp(x_l, t_l) && !Info::cmp(x_r, t_r)) {
             return t;
-        } else if (t_l >= x_l && t_r >= x_r) {
+        } else if (!Info::cmp(t_l, x_l) && !Info::cmp(t_r, x_r)) {
             t->x = x;
             return t;
         } else {
             T m = (l + r) / 2;
             auto t_m = t->x(m), x_m = x(m);
-            if (t_m > x_m) {
+            if (Info::cmp(x_m, t_m)) {
                 std::swap(t->x, x);
-                if (x_l >= t_l)
+                if (!Info::cmp(x_l, t_l))
                     t->l = update(t->l, x, l, m, t_l, t_m);
                 else
                     t->r = update(t->r, x, m + 1, r, t_m + x.a, t_r);
             } else {
-                if (t_l >= x_l)
+                if (!Info::cmp(t_l, x_l))
                     t->l = update(t->l, x, l, m, x_l, x_m);
                 else
                     t->r = update(t->r, x, m + 1, r, x_m + x.a, x_r);
@@ -64,19 +67,36 @@ struct LiChaoTree {
 
     void update(const T& a, const T& b) {
         Line x(a, b);
-        root = update(root, x, x_low, x_high, x(x_low), x(x_high));
+        root = update(root, x, Info::x_low, Info::x_high, x(Info::x_low),
+                      x(Info::x_high));
     }
 
     T query(const Node* t, const T& l, const T& r, const T& x) const {
-        if (!t) return id;
+        if (!t) return Info::id;
         if (l == r) return t->x(x);
         T m = (l + r) / 2;
         if (x <= m)
-            return std::min(t->x(x), query(t->l, l, m, x));
+            return std::min(t->x(x), query(t->l, l, m, x), Info::cmp);
         else
-            return std::min(t->x(x), query(t->r, m + 1, r, x));
+            return std::min(t->x(x), query(t->r, m + 1, r, x), Info::cmp);
     }
 
-    T query(const T& x) const { return query(root, x_low, x_high, x); }
+    T query(const T& x) const {
+        return query(root, Info::x_low, Info::x_high, x);
+    }
 };
+
+using ll = std::int64_t;
+
+struct LiChaoInfo {
+    using T = ll;
+    static constexpr T x_low = -ll(1e9) - 1;
+    static constexpr T x_high = ll(1e9) + 1;
+    // min: 8e18, max: -8e18
+    static constexpr T id = -ll(8e18);
+    // min: x < y, max: x > y
+    static constexpr bool cmp(T x, T y) { return x > y; }
+};
+
+using LiChao = LiChaoTree<LiChaoInfo>;
 
