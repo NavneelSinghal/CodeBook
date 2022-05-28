2768b0d2b5cfb62a5ee88004c709e8fda9aa35d4
Navneel Singhal <navneel.singhal.iitd@gmail.com>
2022-05-28 16:07:32 +0530

    make persistent array and persistent dsu faster by removing std::variant and std::visit

--- ds/persistent_array.cpp
+++ ds/persistent_array.cpp
@@ -1,98 +1,65 @@
 template <class T>
 struct PersistentArray {
-    PersistentArray(const std::vector<T>& v) : d{{v, 0}} {}
+    using Array = std::vector<T>;
+
+    PersistentArray(const Array& data_)
+        : data{data_}, d{{true, -1, T{}, -1, 0}} {}
 
     // returns value of latest version
-    T get(int version, int index) { return d[version].get(index, d); }
+    T get(int version, int index) { return d[version].get(index, d, data); }
 
     // returns value of latest version
-    T get(int index) { return d.back().get(index, d); }
+    T get(int index) { return d.back().get(index, d, data); }
 
     // returns new version number
     int set(int version, int index, T value) {
-        int new_index = (int)d.size();
-        d.push_back(d[version].set(index, value, new_index));
-        return new_index;
+        int new_pos = (int)d.size();
+        d.push_back(d[version].set(index, value, new_pos));
+        return new_pos;
     }
 
     // returns new version number
     int set(int index, T value) {
-        int new_index = (int)d.size();
-        d.push_back(d.back().set(index, value, new_index));
-        return new_index;
+        int new_pos = (int)d.size();
+        d.push_back(d.back().set(index, value, new_pos));
+        return new_pos;
     }
 
     int total_versions() { return (int)d.size(); }
 
    private:
-    struct Wrapper;
-    struct Diff {
+    struct Node;
+    using Container = std::vector<Node>;
+    struct Node {
+        bool is_root;
         int index;
         T value;
-        int parent_index;
-    };
-    using Node = std::variant<std::vector<T>, Diff>;
-    using Container = std::vector<Wrapper>;
-
-    struct Wrapper {
-        Node node;
-        int self_index;
+        int parent_pos;
+        int self_pos;
         // for a single persistent array, the container c should be the same
-        T get(int i, Container& c) {
-            reroot(c);
-            return std::visit(
-                [&](auto&& data) {
-                    using type = std::decay_t<decltype(data)>;
-                    if constexpr (std::is_same_v<type, std::vector<T>>) {
-                        return data[i];
-                    } else {
-                        // assert(false);
-                        // after rerooting, this is unreachable
-                        return T{};
-                    }
-                },
-                node);
+        T get(int i, Container& c, Array& a) {
+            reroot(c, a);
+            return a[i];
         }
-        Wrapper set(int i, T v, int new_self_index) {
-            return Wrapper{Diff{i, v, self_index}, new_self_index};
+        Node set(int i, T v, int new_self_pos) {
+            return Node{false, i, v, self_pos, new_self_pos};
         }
-        void reroot(Container& c) {
-            std::visit(
-                [&](auto&& data) {
-                    using type = std::decay_t<decltype(data)>;
-                    if constexpr (std::is_same_v<type, std::vector<T>>) {
-                        return;
-                    } else {
-                        // index of diff
-                        auto i = data.index;
-                        // value after diff
-                        // using a reference will be UB, so either use boxed T
-                        // or T where copying is cheap
-                        auto v = data.value;
-                        auto parent = data.parent_index;
-                        // reverse linked list
-                        c[parent].reroot(c);
-                        // now set parent as the child of this
-                        node = std::vector<T>{};
-                        std::visit(
-                            [&](auto&& p) {
-                                using type_2 = std::decay_t<decltype(p)>;
-                                if constexpr (std::is_same_v<type_2,
-                                                             std::vector<T>>) {
-                                    std::swap(v, p[i]);
-                                    std::swap(std::get<std::vector<T>>(node),
-                                              p);
-                                } else {
-                                    // assert(false);
-                                }
-                            },
-                            c[parent].node);
-                        c[parent].node = Diff{i, v, self_index};
-                    }
-                },
-                node);
+        void reroot(Container& c, Array& a) {
+            if (!is_root) {
+                auto i = index;
+                auto v = value;
+                auto& p = c[parent_pos];
+                p.reroot(c, a);
+                is_root = true;
+                std::swap(v, a[i]);
+                p.is_root = false;
+                p.index = i;
+                p.value = v;
+                p.parent_pos = self_pos;
+            }
         }
     };
+    Array data;
     Container d;
 };
 
