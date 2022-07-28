2d952fdaaba0b7144ce6a0c0ca8897387f3dd2f1
Navneel Singhal <navneel.singhal.iitd@gmail.com>
2022-07-28 22:14:25 +0530

    fixed LIS

--- misc/lis.cpp
+++ misc/lis.cpp
@@ -1,80 +1,45 @@
 // find the longest subsequence s of [0, ..., n - 1] such that
 // comparator(s[i], s[i + 1]) is true for all valid i
 // no need to use template parameters for strict/non-strict
-template <class Compare, bool return_indices = false, bool do_reserve = false>
-auto get_lis_indices(int n, Compare&& comparator) {
-    std::vector<int> indices;
+template <bool return_indices = false, bool do_reserve = false>
+auto get_lis_indices(int n, auto&& comparator) {
+    std::vector<int> indices, position;
+    if constexpr (return_indices) position = std::vector<int>(n);
     if constexpr (do_reserve) indices.reserve(n);
     for (int i = 0; i < n; ++i) {
-        int location;
-        location = int(
+        auto location = static_cast<int>(
             std::lower_bound(indices.begin(), indices.end(), i, comparator) -
             indices.begin());
-        if (location == (int)indices.size())
+        if constexpr (return_indices) position[i] = location;
+        if (location == static_cast<int>(indices.size()))
             indices.push_back(i);
         else
             indices[location] = i;
     }
-    if constexpr (return_indices)
-        return indices;
-    else
+    if constexpr (return_indices) {
+        auto remaining = static_cast<int>(indices.size());
+        std::vector<int> ans(remaining);
+        for (int i = n - 1; i >= 0; --i)
+            if (position[i] == remaining - 1) ans[--remaining] = i;
+        return ans;
+    } else
         return indices.size();
 }
 
-template <class T, bool is_strict = true, bool return_indices = false,
-          bool do_reserve = false>
-auto get_lis_indices(const std::vector<T>& a) {
-    std::vector<std::size_t> indices;
-    const auto n = a.size();
-    if constexpr (do_reserve) indices.reserve(n);
-    const auto comparator = [&a](const std::size_t& x, const std::size_t& y) {
-        return a[x] < a[y];
-    };
-    for (std::size_t i = 0; i < n; ++i) {
-        std::size_t location;
-        if constexpr (is_strict)
-            location = std::lower_bound(std::begin(indices), std::end(indices),
-                                        i, comparator) -
-                       std::begin(indices);
+template <bool return_values = false, bool do_reserve = false>
+auto get_lis(auto Begin, auto End, auto&& comparator) {
+    std::vector<std::decay_t<decltype(*Begin)>> ans;
+    int n = static_cast<int>(End - Begin);
+    if constexpr (do_reserve) ans.reserve(n);
+    for (int i = 0; Begin != End; ++Begin, ++i) {
+        auto&& element = *Begin;
+        auto location = static_cast<int>(
+            std::lower_bound(ans.begin(), ans.end(), element, comparator) -
+            ans.begin());
+        if (location == static_cast<int>(ans.size()))
+            ans.push_back(element);
         else
-            location = std::upper_bound(std::begin(indices), std::end(indices),
-                                        i, comparator) -
-                       std::begin(indices);
-        if (location == indices.size())
-            indices.push_back(i);
-        else
-            indices[location] = i;
+            ans[location] = element;
     }
-    if constexpr (return_indices)
-        return indices;
-    else
-        return indices.size();
+    return ans;
 }
-
-template <class T, bool is_strict = true, bool return_elements = false,
-          bool do_reserve = false>
-auto get_lis_elements(const std::vector<T>& a) {
-    std::vector<T> elements;
-    const auto n = a.size();
-    if constexpr (do_reserve) elements.reserve(n);
-    for (std::size_t i = 0; i < n; ++i) {
-        std::size_t location;
-        if constexpr (is_strict)
-            location = std::lower_bound(std::begin(elements),
-                                        std::end(elements), a[i]) -
-                       std::begin(elements);
-        else
-            location = std::upper_bound(std::begin(elements),
-                                        std::end(elements), a[i]) -
-                       std::begin(elements);
-        if (location == elements.size())
-            elements.push_back(a[i]);
-        else
-            elements[location] = a[i];
-    }
-    if constexpr (return_elements)
-        return elements;
-    else
-        return elements.size();
-}
-
